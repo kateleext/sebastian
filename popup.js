685dafd5f5d8b2f76374a3d46d708ddbb19c90b0
Kate Lee <kateleext@gmail.com>
2023-08-08 21:06:00 +0200

    additional changes

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById('addBlock').addEventListener('click', function() {
+    var domain = document.getElementById('domain').value;
+    var memeFile = document.getElementById('memeFile').files[0];
+    // TODO: Save the domain and meme to storage
+    // You can use the FileReader API to read the memeFile
+  });
+  
